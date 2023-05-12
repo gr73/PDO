53bc221e06444ffd1ffed2f77585ae7202ea64e3
gr73 <131654013+gr73@users.noreply.github.com>
2023-05-12 14:08:42 +0200

    Add files via upload

---
+++ PHP week-1.php
@@ -0,0 +1,26 @@
+<?php 
+
+$getal=12;
+$getal1=10;
+echo $getal + $getal1;
+
+echo "<br>";
+
+$getal=12;
+$getal1=10;
+echo $getal * $getal1;
+
+echo "<br>";
+
+echo "Welkom Gorav";
+echo "<br>";
+
+// deze code bevat veel fouten
+//haal alle fouten eruit!
+
+
+$mijnNaam="carl";
+$jouwNaam="Camel";
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!";
+
+?>
